bc80314988ee5a6682778edd875be67c2e84f823
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-19 14:50:48 -0400

    more fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,12 +37,14 @@ class EventsController < ApplicationController
   end
 
   def results
-    keys_we_want = ["name", "zip", "boroughs"]
+    keys_we_want = ["keywords", "zip", "boroughs", "categories"]
     url_additions = []
     keys_we_want.each do |key|
       if params[key].present?
         if params[key].class == Array
+          array_url = params[key].join("%2C+").gsub(/[ ]/, '+').gsub(/[&]/, '%26')
           #join the array with whatever (%20+) and shovel into url_additions
+          url_additions << "&#{key}=#{array_url}"
         else
           url_additions << "&#{key}=#{params[key]}"
         end
@@ -51,15 +53,14 @@ class EventsController < ApplicationController
     base_url = "https://api.cityofnewyork.us/calendar/v1/search.htm?app_id=a64cc336&app_key=4b82d0291b83dc1c52ee0f19d9f4f93c"
     #### search_terms SOMETHING GOES HERE ####
     complete_url = base_url + url_additions.join("") + "&sort=DATE"
-    byebug
     response = RestClient.get(complete_url)
     data = JSON.parse(response)
     @results = []
     data["items"].each do |event_data|
-      event = Event.find_or_create_by(name: event_data["name"])
+      event = Event.find_or_create_by(name: event_data["name"], start_time: event_data["startDate"])
       event.name = event_data["name"]
       event.description = event_data["shortDesc"]
-      event.start_time = event_data["startDate"]
+      event.start_time = event_data["startDate"].chomp("-04:00")
       event.end_time = event_data["endDate"]
       event.location = event_data["location"]
       event.borough = event_data["boroughs"] # can contain multiple borough
