02073a9116cca479ae16c5c2ff6837bdc0c8a3c8
github username <github email address>
2017-04-23 16:00:48 -0400

    update user page so past appointments aren't displayed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @events = @user.events.all
+    @upcoming_events = @user.upcoming_events
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   has_many :appointments
   has_many :events, through: :appointments
 
+  def upcoming_events
+    events.all.select do |event|
+      event.start_time > Time.now
+    end
+  end
+
 end
